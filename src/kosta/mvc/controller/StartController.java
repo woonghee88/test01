b19dec45c63a2c75317a116fc9cbe04cd4d341ba
한웅희 <@192.168.0.126>
2020-04-22 10:42:51 +0900

    first commit

---
+++ src/kosta/mvc/controller/StartController.java
@@ -0,0 +1,31 @@
+package kosta.mvc.controller;
+
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StartController
+ */
+@WebServlet("/StartController")
+public class StartController extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
